0664c682be13c915f75b62c2f71cf2b008e7ecd1
prtk_gupta <prateekjpg@gmail.com>
2024-10-06 15:51:22 +0530

    Initial commit

---
+++ client/src/Register.jsx
@@ -0,0 +1,33 @@
+import { useState, useContext } from "react"
+import axios from 'axios'
+import { UserContext } from "./UserContext"
+
+export default function Register() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const {setUsername: setLoggedInUsername, setId} = useContext(UserContext)
+    async function register(e) {
+        e.preventDefault()
+        const {data} = await axios.post('/register', {username, password})
+        setLoggedInUsername(username)
+        setId(data.id)
+    }
+
+    return (
+        <div className="bg-blue-50 h-screen flex items-center">
+            <form className="w-64 mx-auto mb-12" onSubmit={register}>
+                <input 
+                    value={username} 
+                    onChange={e => setUsername(e.target.value)} 
+                    type="text" placeholder="username" 
+                    className="block w-full rounded-sm mb-2 p-2 border" />
+                <input 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)}
+                    type="password" placeholder="password" 
+                    className="block w-full rounded-sm mb-2 p-2 border" />
+                <button className="bg-blue-500 w-full rounded-sm p-2 text-white block">Register</button>
+            </form>
+        </div>
+    )
+}
---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
+const User = require('./models/User');
+dotenv.config()
+const jwtSecret = process.env.JWT_SECRET
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, X-Auth-Token");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+})
+
+const mongo_uri = process.env.MONGO_URL
+mongoose.connect(mongo_uri)
+    .then(() => {
+        console.log('Connected to the Database');
+    })
+    .catch(err => console.error(err));
+
+app.get('/test', (req, res) => {
+    res.json('Test ok')
+})
+
+app.get('/profile', async (req, res) => {
+    const token = req.cookies?.token
+
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+            if (err) throw err
+            res.json(userData)
+        })
+    } else {
+        res.status(401).json('no token')
+    }
+})
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body
+    const createdUser = await User.create({username, password})
+    jwt.sign({userId: createdUser._id, username}, jwtSecret, {}, (err, token) => {
+        if (err) throw err
+        res.cookie('token', token, {sameSite: 'none', secure: true}).status(201).json({
+            id: createdUser._id,
+        })
+    })
+})
+
+app.listen(4040)
