c3dc00adc7b0ab7d8f3cb0d53eaf649f647bfd74
prtk_gupta <prateekjpg@gmail.com>
2024-10-13 20:32:56 +0530

    Add login route to server

--- server/index.js
+++ server/index.js
@@ -1,58 +1,103 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
 
-const User = require('./models/User');
-dotenv.config()
-const jwtSecret = process.env.JWT_SECRET
+const User = require("./models/User");
+dotenv.config();
+const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cookieParser())
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, X-Auth-Token");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-})
+  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, X-Auth-Token"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
-const mongo_uri = process.env.MONGO_URL
-mongoose.connect(mongo_uri)
-    .then(() => {
-        console.log('Connected to the Database');
-    })
-    .catch(err => console.error(err));
+const mongo_uri = process.env.MONGO_URL;
+mongoose
+  .connect(mongo_uri)
+  .then(() => {
+    console.log("Connected to the Database");
+  })
+  .catch((err) => console.error(err));
 
-app.get('/test', (req, res) => {
-    res.json('Test ok')
-})
+app.get("/test", (req, res) => {
+  res.json("Test ok");
+});
 
-app.get('/profile', async (req, res) => {
-    const token = req.cookies?.token
+app.get("/profile", async (req, res) => {
+  const token = req.cookies?.token;
 
-    if (token) {
-        jwt.verify(token, jwtSecret, {}, (err, userData) => {
-            if (err) throw err
-            res.json(userData)
-        })
-    } else {
-        res.status(401).json('no token')
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, (err, userData) => {
+      if (err) throw err;
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json("no token");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
+  if (foundUser) {
+    const passOk = bcrypt.compareSync(password, foundUser.password);
+    if (passOk) {
+      jwt.sign(
+        { userId: foundUser._id, username },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res
+            .cookie("token", token, { sameSite: "none", secure: true })
+            .status(201)
+            .json({
+              id: foundUser._id,
+            });
+        }
+      );
     }
-})
+  }
+});
 
-app.post('/register', async (req, res) => {
-    const {username, password} = req.body
-    const createdUser = await User.create({username, password})
-    jwt.sign({userId: createdUser._id, username}, jwtSecret, {}, (err, token) => {
-        if (err) throw err
-        res.cookie('token', token, {sameSite: 'none', secure: true}).status(201).json({
-            id: createdUser._id,
-        })
-    })
-})
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+  const createdUser = await User.create({
+    username,
+    password: hashedPassword,
+  });
+  jwt.sign(
+    { userId: createdUser._id, username },
+    jwtSecret,
+    {},
+    (err, token) => {
+      if (err) throw err;
+      res
+        .cookie("token", token, { sameSite: "none", secure: true })
+        .status(201)
+        .json({
+          id: createdUser._id,
+        });
+    }
+  );
+});
 
-app.listen(4040)
+app.listen(4040);
