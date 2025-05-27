61cf9a650360290bffe530da7722b151fabf5058
Prateek Gupta <prateekjpg@gmail.com>
2025-05-27 15:52:50 +0530

    Minor changes to api endpoints

--- api/index.js
+++ api/index.js
@@ -55,11 +55,11 @@ async function getUserDataFromRequest(req) {
 
 }
 
-app.get('/test', (req,res) => {
+app.get('/api/test', (req,res) => {
   res.json('test ok');
 });
 
-app.get('/messages/:userId', async (req,res) => {
+app.get('/api/messages/:userId', async (req,res) => {
   const {userId} = req.params;
   const userData = await getUserDataFromRequest(req);
   const ourUserId = userData.userId;
@@ -70,12 +70,12 @@ app.get('/messages/:userId', async (req,res) => {
   res.json(messages);
 });
 
-app.get('/people', async (req,res) => {
+app.get('/api/people', async (req,res) => {
   const users = await User.find({}, {'_id':1,username:1});
   res.json(users);
 });
 
-app.get('/profile', (req,res) => {
+app.get('/api/profile', (req,res) => {
   const token = req.cookies?.token;
   if (token) {
     jwt.verify(token, jwtSecret, {}, (err, userData) => {
@@ -87,7 +87,7 @@ app.get('/profile', (req,res) => {
   }
 });
 
-app.post('/login', async (req,res) => {
+app.post('/api/login', async (req,res) => {
   const {username, password} = req.body;
   const foundUser = await User.findOne({username});
   if (foundUser) {
@@ -102,11 +102,11 @@ app.post('/login', async (req,res) => {
   }
 });
 
-app.post('/logout', (req,res) => {
+app.post('/api/logout', (req,res) => {
   res.cookie('token', '', {sameSite:'none', secure:true}).json('ok');
 });
 
-app.post('/register', async (req,res) => {
+app.post('/api/register', async (req,res) => {
   const {username,password} = req.body;
   try {
     const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
